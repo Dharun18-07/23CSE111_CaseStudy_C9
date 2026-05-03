b9c344d994674574d6a4dfe61d9f157d880004fe
Nithilan-su57 <nithilan1207@gmail.com>
2026-05-03 13:42:09 +0530

    Fix case sensitivity in findMemberById method

--- src/GymManager.java
+++ src/GymManager.java
@@ -84,7 +84,7 @@ public class GymManager{
 
     public void findMemberById(String id){
         for(int i=0;i<memberCount;i++){
-            if(members[i]!=null&&members[i].getId().equals(id)){
+            if(members[i]!=null&&members[i].getId().equals(id).equalsIgnoreCase()){
                 members[i].displayProfile();
                 return;
             }
